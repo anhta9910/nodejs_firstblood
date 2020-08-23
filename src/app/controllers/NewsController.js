78361895d1b8671ca90bc045b918f4a5f7fa2c1f
Ta Hong Anh <Ta Anh>
2020-08-24 05:36:57 +0700

    install prettier,lint-staged,husky

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,14 +1,13 @@
 class NewsController {
-    
     //[GET] /news
     index(req, res) {
         res.render('news');
     }
 
     //[GET] /news/:slug
-    show(req,res) {
+    show(req, res) {
         res.send('NewDetail');
     }
 }
 
-module.exports = new NewsController;
+module.exports = new NewsController();
