cd8a0cc3b608d5841bb123d41218f26ddc74941f
Ta Hong Anh <Ta Anh>
2020-09-02 17:57:17 +0700

    update-course

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
+const meRouter = require('./me');
 const coursesRouter = require('./courses');
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 
 function route(app) {
     app.use('/news', newsRouter);
-
+    app.use('/me', meRouter);
     app.use('/courses', coursesRouter);
 
     app.use('/', siteRouter);
